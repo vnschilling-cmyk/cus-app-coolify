fa94375e97d7b2b3646a82473ce60e997f9846b5
Schilling <vschilling@teko-gmbh.com>
2026-01-30 13:55:36 +0100

    feat: set default theme to TEKO

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 enum AppThemeMode { system, light, dark, teko }
 
 class ThemeNotifier extends StateNotifier<AppThemeMode> {
-  ThemeNotifier() : super(AppThemeMode.system);
+  ThemeNotifier() : super(AppThemeMode.teko);
 
   void setThemeMode(AppThemeMode mode) {
     state = mode;
